d755626f9ca8d364175dcb194bc51cdde28ea3e7
Nico Deblauwe <nico@bluepundit.eu>
2023-12-05 11:30:52 +0100

    REFACTOR :hammer: Better placing of layouts

--- app/View/Components/SiteLayoutNavbar.php
+++ app/View/Components/SiteLayoutNavbar.php
@@ -24,6 +24,6 @@ class SiteLayoutNavbar extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.site-layout-navbar');
+        return view('layouts.site-navbar');
     }
 }
